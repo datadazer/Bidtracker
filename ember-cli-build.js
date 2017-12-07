4a9c5b8340194bcc0ac4639cf8f31c6e27073009
Tyler Davis <tylerdavis@Tylers-MacBook-Pro.local>
2017-12-07 11:28:29 -0700

    first commit setup

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('node_modules/jquery/dist/jquery.slim.min.js');
+  app.import('node_modules/popper.js/dist/umd/popper.min.js');
+  app.import('node_modules/bootstrap/dist/js/bootstrap.js');
+  app.import('node_modules/bootstrap/dist/css/bootstrap.css');
 
   return app.toTree();
 };
