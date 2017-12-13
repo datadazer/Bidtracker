fc3a8e57cbc2afe8cf532cd8c718756a82dfce58
Tyler Davis <datadazer@gmail.com>
2017-12-13 12:12:21 -0800

    fix rails
    - remove rails-api gem since rails has it built in
    - comment out gem listen of dev environment gemfile

--- bidtracker-api/config/initializers/secret_token.rb
+++ bidtracker-api/config/initializers/secret_token.rb
@@ -16,4 +16,4 @@
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-BidtrackerApi::Application.config.secret_token = 'ab1ea568ce391948ac3933e3c3ab3070717bdcc14cf3a8ee7873dafa525312c2273cb5e005183622e06e2c42ebe613664babf552b89655a8f71cd0cad4b7cdab'
+BidtrackerApi::Application.config.secret_token = 'b63724e29d864561d8333326a7abf4edd3478de37d7e346f9bf6f566f67cea117ec1f6edf5b51932aaccd5b8e8a836fa4979ba23cb388af21fac490ee6bc73a8'
