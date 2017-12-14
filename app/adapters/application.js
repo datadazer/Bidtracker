8e5f7fc31ce06b5e86cbf2cf4d8a79345dd833e5
Tyler Davis <datadazer@gmail.com>
2017-12-13 19:53:39 -0800

    finally get ember proxied for talking json with rails

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,15 +1,16 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
-// import Inflector from 'ember-inflector';
-// import { singularize, pluralize } from 'ember-inflector';
+import { underscore } from '@ember/string';
+import Inflector from 'ember-inflector';
+import { singularize, pluralize } from 'ember-inflector';
 
 // const { String: { underscore } } = Ember;
 
 export default JSONAPIAdapter.extend({
 
-  // pathForType(type) {
-  //   return pluralize(underscore(type));
-  // }
+  pathForType(type) {
+    return pluralize(underscore(type));
+  }
 
 });
