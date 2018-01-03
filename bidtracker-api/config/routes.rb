fdbb02a09b7e9e7e0b6747738858ccfcdaa55e0b
Tyler Davis <datadazer@gmail.com>
2018-01-03 12:04:42 -0800

    fix signet authorization

--- bidtracker-api/config/routes.rb
+++ bidtracker-api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post '/oauth2/token', to: 'tokens#create'
+  post '/token', to: 'tokens#create'
   resources :users, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
