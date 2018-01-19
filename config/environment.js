6a36d1bf0845e82dff99333d9d754ba5476e841b
Tyler Davis <datadazer@gmail.com>
2018-01-18 16:57:56 -0800

    try matching new uri in environment

--- config/environment.js
+++ config/environment.js
@@ -75,7 +75,7 @@ module.exports = function(environment) {
           // apiKey: "688090261241-kllc2v7q8ptn376d92tk84880efvn01f.apps.googleusercontent.com",
           apiKey: "688090261241-0vek9b56e4ngmqgm78nu678u2ul0fme2.apps.googleusercontent.com",
           // redirectUri: "http://localhost:4200",
-          redirectUri: "https://bidtracker.herokuapp.com",
+          redirectUri: "https://bidtracker.herokuapp.com/oauth",
           // should be doing something like this: redirectUri: "http://localhost:4200/torii/redirect.html",
           tokenExchangeUri: "https://bidtracker.herokuapp.com/token"
           // tokenExchangeUri: "http://localhost:3000/token"
